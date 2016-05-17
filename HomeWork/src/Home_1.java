a6765ce4c2c63bc802cc263e983b1a57927cf6e2
javase <java.se@mail.ru>
2016-05-17 22:54:39 +0200

    0.0.2

--- HomeWork/src/Home_1.java
+++ HomeWork/src/Home_1.java
@@ -3,7 +3,6 @@
  */
 public class Home_1 {
     public static void main(String[] args) {
-        String s = "Hi, I'm your first program! Stay tuned!";
-        System.out.println(s);
+        System.out.println("Hi, I'm your first program! Stay tuned!");
     }
 }
